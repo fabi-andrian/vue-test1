10b42958e166162df849ea75023c947d06fc7137
fabi-andrian <andrianirinafabrice2024@gmail.com>
2026-05-02 17:37:55 +0300

    Add new simple page

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Hi {{ firstName.toUpperCase() }} !</h1>
+  <p>You are the GOAT.</p>
+</template>
+
+<script setup>
+  const firstName = "Leo";
+</script>
+
+<style>
+h1 {
+  color: pink;
+}
+p {
+  font-size: 4ch;
+}
+</style>
