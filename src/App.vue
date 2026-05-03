dfea9c4304d08fc27bb53c6988cce85b70722681
fabi-andrian <andrianirinafabrice2024@gmail.com>
2026-05-03 12:26:21 +0300

    test boucle liste movies

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,22 @@
 <p>Compteur : {{ count }}</p>
 <button @click="increment">Incrémenter</button>
 <button @click="decrement">Décrémenter</button>
+<ul>
+  <li v-for="movie in movies">
+    {{ movie }}
+  </li>
+</ul>
 </template>
 
 <script setup>
 import {ref} from 'vue'
 
 const count = ref(0)
+const movies = ref([
+  'John Wick',
+  'Karate Kid',
+  'Titanic'
+])
 const increment = (event) => {
   console.log(event)
   count.value++
