d470eebf880565fa64f6d211f176a62d23cfdd44
fabi-andrian <andrianirinafabrice2024@gmail.com>
2026-05-03 12:13:23 +0300

    test html

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<p :class="{active: count > 5}">Compteur : {{ count }}</p>
+<p>Compteur : {{ count }} <div v-html="firstName"></div></p>
 <button @click="increment">Incrémenter</button>
 <button @click="decrement">Décrémenter</button>
 </template>
@@ -8,7 +8,7 @@
 import {ref} from 'vue'
 
 const count = ref(0)
-console.log(count, count.value)
+const firstName = '<span>Demo</span>'
 const increment = (event) => {
   console.log(event)
   count.value++
