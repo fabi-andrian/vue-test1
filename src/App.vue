fedb80e78d682e96dc31cb26e3ed6d4efa9d7bf4
fabi-andrian <andrianirinafabrice2024@gmail.com>
2026-05-03 11:21:29 +0300

    test Compteur V3

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
 <p>Compteur : {{ count }}</p>
+<div v-show="count>=5">Bravo vous avez cliqué plus de 5 fois</div>
 <button @click="increment">Incrémenter</button>
 <button @click="decrement">Décrémenter</button>
 </template>
@@ -8,7 +9,9 @@
 import {ref} from 'vue'
 
 const count = ref(0)
-const increment = () => {
+console.log(count, count.value)
+const increment = (event) => {
+  console.log(event)
   count.value++
 }
 const decrement = () => {
